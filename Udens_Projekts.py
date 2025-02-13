899eafa3ba1df35f005096f21f2d0ec6a608231a
Lidlll23 <liva.ievina09@gmail.com>
2025-02-13 20:28:43 +0200

    Update Udens_Projekts.py

--- Udens_Projekts.py
+++ Udens_Projekts.py
@@ -24,5 +24,38 @@ if patud<10:
     print("Jūs esat eko draudzīgi!")
 else:
     print("Jūs neesat eko draudzīgi!")
+    
+izvele=input("Vai gribi izmantot programmu atkal? (jā/nē):")
 
+while True:
+    if izvele=="jā":
+        udens=float(input("Ievadi savu ikdienas ūdens patēriņu (litros):"))
+
+        udensD=udens
+        udensM=udens*30
+        udensG=udens*365
+
+        print(f"Tu vidēji iztērē {udensD}l ūdens dienā, {udensM}l mēnesī un {udensG}l gadā.")
+
+        skd=int(input("Ivadi skolēnu skaitu:"))
+        skvid=float(input("Ivadi 1 skolēna vidējo saldūdens patēriņu 1 dienā:"))
+
+        skvidG=skd*skvid*365
+
+        visud=37017300000000000000
+
+        patud=skvidG/visud*100
+
+        print(f"Tava klase vidēji gadā iztērē {patud}% no visām pasaules saldūdens krātuvēm!")
+
+        if patud>10:
+            print("Jūs esat eko draudzīgi!")
+        else:
+            print("Jūs neesat eko draudzīgi!")
+
+        izvele=input("Vai gribi izmantot programmu atkal? (jā/nē):")
+    else:
+        print("Paldies par programmas izmantošanu!")
+        break
+ 
 #Uzlabosim tekstu (kopumā)
