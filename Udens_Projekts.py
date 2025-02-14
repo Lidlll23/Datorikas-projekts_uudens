29905dbef2b6069faa450af7bd2d0ee9a45fb85f
Lidlll23 <liva.ievina09@gmail.com>
2025-02-14 08:44:28 +0200

    Update Udens_Projekts.py

--- Udens_Projekts.py
+++ Udens_Projekts.py
@@ -3,12 +3,14 @@ udens=float(input("Ievadi savu ikdienas ūdens patēriņu (litros):"))
 udensD=udens
 udensM=udens*30
 udensG=udens*365
+#Šeit arī varētu ielikt datu krātuvi (prioritāte)
 
 print(f"Tu vidēji iztērē {udensD}l ūdens dienā, {udensM}l mēnesī un {udensG}l gadā.")
 
 skd=int(input("Ivadi skolēnu skaitu:"))
 skvid=float(input("Ivadi 1 skolēna vidējo saldūdens patēriņu 1 dienā:"))
-#Optimizēšu/mainīšu 9. un 10. rindiņu 
+#Optimizēšu/mainīšu 9. un 10. rindiņu (prioritāte)
+#Varbūt šeit ielikšu datu krātuvi (prioritāte)
 
 skvidG=skd*skvid*365
 
@@ -16,11 +18,12 @@ visud=37017300000000000000
 
 patud=skvidG/visud*100
 #Neesmu pārliecināta par šo procentu rēķināšanas metodi
+#Kaut kas aprēķinos ir nepareizs, ievadot mazāku skd un skvid, procentu skaits patud palielinās. (prioritāte, viss lielākā)
 
 print(f"Tava klase vidēji gadā iztērē {patud}% no visām pasaules saldūdens krātuvēm!")
 
 if patud<10:
-    #Precizēšu pieļaujamos procentus vēlāk. (pašlaiku paliks 10%)
+    #Precizēšu pieļaujamos procentus vēlāk, pašlaiku paliks 10% (šī būs prioritāte pēc aprēķinu salabošanas)
     print("Jūs esat eko draudzīgi!")
 else:
     print("Jūs neesat eko draudzīgi!")
@@ -57,5 +60,7 @@ while True:
     else:
         print("Paldies par programmas izmantošanu!")
         break
+    #Ja atradīšu kā, tad optimizēšu šo ciklu (šī nav prioritāte)
  
-#Uzlabosim tekstu (kopumā)
+#Uzlabosim tekstu (kopumā) (šī nav prioritāte)
+#Vajag izmantot datu krātuves(google tutorials) (veseli 2 punkti, prioritāte)
